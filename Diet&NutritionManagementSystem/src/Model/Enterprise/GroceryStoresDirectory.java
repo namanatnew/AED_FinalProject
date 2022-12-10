501cbe4c4328ab047664ca9c4bea36d7b974bac9
VipulR2709 <rajderkar.v@northeastern.edu>
2022-12-10 00:20:23 -0500

    conflicts resolved

--- Diet&NutritionManagementSystem/src/Model/Enterprise/GroceryStoresDirectory.java
+++ Diet&NutritionManagementSystem/src/Model/Enterprise/GroceryStoresDirectory.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model.Enterprise;
+package Model.Organisation;
 
 //import Model.Account.Account;
 import Model.Database.DBconnection;
@@ -132,7 +132,7 @@ public class GroceryStoresDirectory {
             System.out.print("found it");
             return res;
         } catch (SQLException ex) {
-            Logger.getLogger(ProductDirectory.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(GroceryStoresDirectory.class.getName()).log(Level.SEVERE, null, ex);
         }
             return null;
     }
